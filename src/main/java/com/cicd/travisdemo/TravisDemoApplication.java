c9ba48129d279f24dc08db02395c7832dd55d4c2
vinothkumar <net.vinothkumar@gmail.com>
2020-02-14 14:30:01 +0100

    travis cicd

---
+++ src/main/java/com/cicd/travisdemo/TravisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.travisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/travisdemo/TravisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.travisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
